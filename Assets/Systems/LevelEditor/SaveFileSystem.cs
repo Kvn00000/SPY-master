d71918348f63215570ef9516bcb683b36fa0be2d
Kvn00000 <94387120+Kvn00000@users.noreply.github.com>
2024-10-22 23:31:29 +0200

    Update SaveFileSystem.cs

--- Assets/Systems/LevelEditor/SaveFileSystem.cs
+++ Assets/Systems/LevelEditor/SaveFileSystem.cs
@@ -243,7 +243,7 @@ public class SaveFileSystem : FSystem
 					levelExport += "\t</console>\n\n";
 					break;
 				case Jerrycan j:
-					levelExport += "\t<oilcontainer posX=\""+(j.col+1 - minCol)  + "\" posY=\"" + (j.line+ 1 - minLine) + "\" oilqte=\"" + j.oilQuantity+ "\" />\n\n";
+					levelExport += "\t<oilcontainer posX=\"" + (j.col+1 - minCol) + "\" posY=\"" + (j.line+ 1 - minLine) + "\" oilqte=\"" + 5 + "\"" + " direction=\"" + 2 + "\" />\n";
 					break;
 				case Door d:
 					levelExport += "\t<door posX=\"" + (d.col+1 - minCol) + "\" posY=\"" + (d.line+ 1 - minLine) + "\" slotId=\""+ d.slot + "\" direction=\""+ (int)d.orientation + "\" />\n\n";
